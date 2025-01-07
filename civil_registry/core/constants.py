473636ec031aaae286b20e794f48b48ca3a87f16
IMperiumX <yusufadell.dev@gmail.com>
2025-01-08 00:32:28 +0200

    Add constants for century digits and governorates mapping

---
+++ civil_registry/core/constants.py
@@ -0,0 +1,33 @@
+MIN_CENTURY_DIGIT = 2
+MAX_CENTURY_DIGIT = 3
+
+GOVERNORATES_MAPPING = {
+    "01": "Cairo",
+    "02": "Alexandria",
+    "03": "Port Said",
+    "04": "Suez",
+    "11": "Damietta",
+    "12": "Dakahlia",
+    "13": "Ash Sharqia",
+    "14": "Kaliobeya",
+    "15": "Kafr El Sheikh",
+    "16": "Gharbia",
+    "17": "Monufia",
+    "18": "Qalyubia",
+    "19": "Sharqia",
+    "21": "Giza",
+    "22": "Beni Suef",
+    "23": "Fayoum",
+    "24": "Minya",
+    "25": "Asyut",
+    "26": "Sohag",
+    "27": "Qena",
+    "28": "Luxor",
+    "29": "Aswan",
+    "31": "Red Sea",
+    "32": "New Valley",
+    "33": "Matrouh",
+    "34": "North Sinai",
+    "35": "South Sinai",
+    "88": "Foreign",
+}
