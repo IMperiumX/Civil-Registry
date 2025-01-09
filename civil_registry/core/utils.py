160d765ea5c012cfdc696ad13bf3414efc3df0ff
IMperiumX <yusufadell.dev@gmail.com>
2025-01-09 05:35:32 +0200

    Add utility function for MD5 hashing of text inputs

---
+++ civil_registry/core/utils.py
@@ -0,0 +1,12 @@
+import hashlib
+from hashlib import md5 as _md5
+from typing import Any
+
+from django.utils.encoding import force_bytes
+
+
+def md5_text(*args: Any) -> hashlib._Hash:
+    m = _md5()  # noqa: S324
+    for x in args:
+        m.update(force_bytes(x, errors="replace"))
+    return m
