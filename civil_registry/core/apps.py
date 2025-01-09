6fe1f76618a2ead6db03aa71a820b8c0949736ae
IMperiumX <yusufadell.dev@gmail.com>
2025-01-09 05:26:49 +0200

    Configure structured logging on app ready

--- civil_registry/core/apps.py
+++ civil_registry/core/apps.py
@@ -2,6 +2,8 @@ import contextlib
 
 from django.apps import AppConfig
 
+from civil_registry.core.loggers import configure_structlog
+
 
 class CoreConfig(AppConfig):
     name = "civil_registry.core"
@@ -9,3 +11,4 @@ class CoreConfig(AppConfig):
     def ready(self):
         with contextlib.suppress(ImportError):
             import civil_registry.core.signals  # noqa: F401
+        configure_structlog()
