7170dc76d6902ae1e02c457282f90438dac52fe9
IMperiumX <yusufadell.dev@gmail.com>
2025-01-10 06:13:36 +0200

    Add freeze_time utility function to facilitate time manipulation in tests

--- civil_registry/core/utils.py
+++ civil_registry/core/utils.py
@@ -1,6 +1,11 @@
+from __future__ import annotations
+
+from datetime import datetime
+from datetime import timezone
 from hashlib import md5 as _md5
 from typing import Any
 
+import time_machine
 from django.utils.encoding import force_bytes
 
 
@@ -9,3 +14,9 @@ def md5_text(*args: Any):
     for x in args:
         m.update(force_bytes(x, errors="replace"))
     return m
+
+
+def freeze_time(t: str | datetime | None = None) -> time_machine.travel:
+    if t is None:
+        t = datetime.now(timezone.UTC)
+    return time_machine.travel(t, tick=False)
