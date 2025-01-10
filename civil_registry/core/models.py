3d21ea805d514735342fe399ec1a067145e59d8a
IMperiumX <yusufadell.dev@gmail.com>
2025-01-10 06:10:44 +0200

    Refactor National ID validation and enhance ApiCall model with additional fields

--- civil_registry/core/models.py
+++ civil_registry/core/models.py
@@ -1,17 +1,16 @@
 import datetime
 import logging
+import re
 from abc import ABC
 from abc import abstractmethod
 from dataclasses import dataclass
 
-from django.conf import settings
 from django.db import models
 
 from .constants import GOVERNORATES_MAPPING
-from .constants import MAX_CENTURY_DIGIT
-from .constants import MIN_CENTURY_DIGIT
 from .exceptions import InvalidBirthDateError
 from .exceptions import InvalidCenturyDigitError
+from .exceptions import InvalidNationalIDError
 
 logger = logging.getLogger("core")
 
@@ -24,13 +23,19 @@ class NationalID(ABC):
     gender: str = None
 
     def __post_init__(self):
-        logger.debug(f"Creating {self}")
-        # Ensure the ID is a string
-        object.__setattr__(self, "id_number", str(self.id_number))
+        logger.debug("Extracting information from the National ID %s", self)
+        # Ensure the ID is valid
+        self.validate()
+
         # Extract the information from the National ID
         object.__setattr__(self, "birth_date", self.extract_birth_date())
         object.__setattr__(self, "governorate", self.extract_governorate())
         object.__setattr__(self, "gender", self.extract_gender())
+        logger.debug("National ID information extracted successfully %s", self)
+
+    @abstractmethod
+    def validate(self):
+        """Validates the National ID."""
 
     @abstractmethod
     def extract_birth_date(self):
@@ -50,28 +55,39 @@ class EgyptianNationalID(NationalID):
     Represents an Egyptian National ID.
 
     The ID is a 14-digit number with the following format:
-        - 1st digit: Century digit (1 for 19XX, 2 for 20XX)
-        - 2nd-3rd digits: Year of birth
-        - 4th-5th digits: Month of birth
-        - 6th-7th digits: Day of birth
+        - 1st digit: Century digit (2 for 19XX, 3 for 20XX)
+        - 2nd-7th digits: Date of birth (YYMMDD)
         - 8th-9th digits: Governorate code
-        - 10th-13th digits: Random number
+        - 10th-13th digits: Serial number
+        - 14th digit: Check digit
     ref: https://en.wikipedia.org/wiki/Egyptian_National_Identity_Card
     """
 
     ID_LENGTH = 14
+    ID_REGEX = re.compile(r"^\d{14}$")  # 14-digit number
+
+    MIN_CENTURY_DIGIT = 2
+    MAX_CENTURY_DIGIT = 3
+
+    def validate(self):
+        """Validates the Egyptian National ID."""
+        id_number = re.match(self.ID_REGEX, str(self.id_number))
+        if not id_number:
+            raise InvalidNationalIDError(self.id_number, self.ID_LENGTH)
+
+        object.__setattr__(self, "id_number", id_number.string)
 
     def extract_birth_date(self):
         """Extracts the birth date from the National ID."""
         century_digit = int(self.id_number[0])
-        if century_digit not in (MIN_CENTURY_DIGIT, MAX_CENTURY_DIGIT):
+        if century_digit not in (self.MIN_CENTURY_DIGIT, self.MAX_CENTURY_DIGIT):
             raise InvalidCenturyDigitError(century_digit)
 
         year = int(self.id_number[1:3])
         month = int(self.id_number[3:5])
         day = int(self.id_number[5:7])
 
-        if century_digit == MIN_CENTURY_DIGIT:
+        if century_digit == self.MIN_CENTURY_DIGIT:
             year += 1900
         else:
             year += 2000
@@ -92,27 +108,23 @@ class EgyptianNationalID(NationalID):
         return "Female" if gender_digit % 2 == 0 else "Male"
 
 
-class APICall(models.Model):
-    # relations
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-    )
-    # fields
-    endpoint = models.CharField(max_length=255)
-    id_number = models.CharField(max_length=14, blank=True, default="")
-    request_data = models.JSONField(
-        null=True,
-        blank=True,
-    )  # Store request data (optional)
-    response_data = models.JSONField(
-        null=True, blank=True
-    )  # Store response data (optional)
-    status_code = models.IntegerField(null=True, blank=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+class ApiCall(models.Model):
+    id_number = models.CharField(max_length=14, default="")
     detail = models.TextField(blank=True, default="")
 
+    timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
+    request_id = models.UUIDField(null=True, blank=True, db_index=True)
+    request_method = models.CharField(max_length=10)
+    path = models.CharField(max_length=255, db_index=True)
+    user_id = models.IntegerField(null=True, blank=True, db_index=True)
+    status_code = models.IntegerField()
+    client_ip = models.GenericIPAddressField(null=True, blank=True)
+    user_agent = models.CharField(max_length=255, blank=True, default="")
+
+    class Meta:
+        ordering = ["-timestamp"]
+
     def __str__(self):
-        return f"{self.timestamp} - {self.endpoint} - {self.status_code}"
+        return (
+            f"{self.timestamp} - {self.request_method} {self.path} - {self.status_code}"
+        )
