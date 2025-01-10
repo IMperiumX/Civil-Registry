a0da799a3d2fc376d9030eb88e671d5d24df3c23
IMperiumX <yusufadell.dev@gmail.com>
2025-01-10 06:14:50 +0200

    Configure Redis cache backend and update Celery task settings in local settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,6 +1,5 @@
 from .base import *  # noqa: F403
 from .base import INSTALLED_APPS
-from .base import MIDDLEWARE
 from .base import env
 
 # GENERAL
@@ -20,8 +19,14 @@ ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-        "LOCATION": "",
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": REDIS_URL,  # noqa: F405
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            # Mimicking memcache behavior.
+            # https://github.com/jazzband/django-redis#memcached-exceptions-behavior
+            "IGNORE_EXCEPTIONS": True,
+        },
     },
 }
 
@@ -30,3 +35,9 @@ CACHES = {
 # ------------------------------------------------------------------------------
 # https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
 INSTALLED_APPS += ["django_extensions"]
+
+# Celery
+# ------------------------------------------------------------------------------
+
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-eager-propagates
+CELERY_TASK_EAGER_PROPAGATES = True
